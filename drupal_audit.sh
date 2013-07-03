13c946fa81f8bdef73ede14280396d8d0c842151
Simon Georges <simon.georges@makina-corpus.com>
2013-07-03 18:32:00 +0200

    Cleaner section separation; only useful phpinfo (not sure about ENV and VARS, though)

--- drupal_audit.sh
+++ drupal_audit.sh
@@ -10,11 +10,13 @@ DRUPAL_VERSION = `drush status | grep 'Drupal version' | cut -d: -f2 | sed -e s/
 echo "Start Audit report" > drupal_audit_report.txt
 
 echo "----------------------------------------" >> drupal_audit_report.txt
-echo "Drush status" >> drupal_audit_report.txt
+echo "  Drush status" >> drupal_audit_report.txt
+echo "----------------------------------------" >> drupal_audit_report.txt
 drush status >> drupal_audit_report.txt
 
 echo "----------------------------------------" >> drupal_audit_report.txt
-echo "Hacked status" >> drupal_audit_report.txt
+echo "  Hacked status" >> drupal_audit_report.txt
+echo "----------------------------------------" >> drupal_audit_report.txt
 drush dl -y hacked
 drush en -y hacked
 drush cc drush
@@ -23,38 +25,43 @@ drush dis -y hacked
 drush pm-uninstall -y hacked
 
 echo "----------------------------------------" >> drupal_audit_report.txt
-echo "Custom installation profiles" >> drupal_audit_report.txt
+echo "  Custom installation profiles" >> drupal_audit_report.txt
+echo "----------------------------------------" >> drupal_audit_report.txt
 ls profiles/ | grep -v minimal | grep -v standard | grep -v testing >> drupal_audit_report.txt
 
 echo "----------------------------------------" >> drupal_audit_report.txt
-echo "Makefile(s)" >> drupal_audit_report.txt
+echo "  Makefile(s)" >> drupal_audit_report.txt
+echo "----------------------------------------" >> drupal_audit_report.txt
 find profiles/ -name '*make*' >> drupal_audit_report.txt
 find profiles/ -name '*make*' -exec cat '{}' \; >> drupal_audit_report.txt
 
 echo "----------------------------------------" >> drupal_audit_report.txt
-echo "Module status" >> drupal_audit_report.txt
+echo "  Modules status" >> drupal_audit_report.txt
+echo "----------------------------------------" >> drupal_audit_report.txt
 drush pml >> drupal_audit_report.txt
 
 echo "----------------------------------------" >> drupal_audit_report.txt
-echo "Themes status" >> drupal_audit_report.txt
+echo "  Themes status" >> drupal_audit_report.txt
+echo "----------------------------------------" >> drupal_audit_report.txt
 for i in `drush pml | grep Theme | grep Enabled | cut -d\( -f2 | cut -d\) -f1`
 do
   THEMENAME=$i
   THEMEPATH=`drush php-eval "echo drupal_get_path('theme', '$i');"`
   echo "# Theme: $THEMENAME" >> drupal_audit_report.txt
   COUNT=`find $THEMEPATH -name *.tpl.php | wc -l`
-  echo "Templates count: $COUNT" >> drupal_audit_report.txt
+  echo "  Templates count: $COUNT" >> drupal_audit_report.txt
   COUNT=`grep -r function $THEMEPATH | wc -l`
-  echo "Functions count: $COUNT" >> drupal_audit_report.txt
+  echo "  Functions count: $COUNT" >> drupal_audit_report.txt
   COUNT=`grep -r db_[a-z_]*\( $THEMEPATH | wc -l`
-  echo "Database access count: $COUNT" >> drupal_audit_report.txt
+  echo "  Database access count: $COUNT" >> drupal_audit_report.txt
 done
 
 #
 ### CAUTION: This step should only be performed on Drupal 7 websites.
 #
 echo "----------------------------------------" >> drupal_audit_report.txt
-echo "Caches status" >> drupal_audit_report.txt
+echo "  Caches status" >> drupal_audit_report.txt
+echo "----------------------------------------" >> drupal_audit_report.txt
 drush dl -y cacheaudit
 drush en -y cacheaudit
 drush cc drush
@@ -63,8 +70,9 @@ drush dis -y cacheaudit
 drush pm-uninstall -y cacheaudit
 
 echo "----------------------------------------" >> drupal_audit_report.txt
-echo "PHP status" >> drupal_audit_report.txt
-drush php-eval 'phpinfo();' >> drupal_audit_report.txt 
+echo "  PHP status" >> drupal_audit_report.txt
+echo "----------------------------------------" >> drupal_audit_report.txt
+drush php-eval 'phpinfo(INFO_GENERAL | INFO_CONFIGURATION | INFO_MODULES | INFO_ENVIRONMENT | INFO_VARIABLES);' >> drupal_audit_report.txt
 
 # Missing in the script (to do manually):
 # Browser visit with WAppalyser and YSlow extensions
